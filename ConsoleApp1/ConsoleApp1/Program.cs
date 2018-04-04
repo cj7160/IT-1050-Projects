af8f6c7ea83a7bddba62be3056e9893b22fe23dc
cj7160 <35666779+cj7160@users.noreply.github.com>
2018-04-03 20:32:24 -0400

    I am awesome
    I am awesome

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string teamname;
+            string playername;
+
+            float match1kills;
+            float match2kills;
+            float match1deaths;
+            float match2deaths;
+
+            
+
+            System.Console.Write("Input team name. ");
+            teamname = System.Console.ReadLine();
+
+            System.Console.Write("Input player name. ");
+            playername = System.Console.ReadLine();
+
+            System.Console.Write("Input match 1 kills. ");
+            match1kills = float.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Input match 1 deaths.");
+            match1deaths = float.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Input match 2 kills. ");
+            match2kills = float.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Input match 2 deaths. ");
+            match2deaths = float.Parse(System.Console.ReadLine());
+
+            float weekKD = (match1kills + match2kills) / (match1deaths + match2deaths);
+
+            System.Console.WriteLine("Weekly K/D results for " + playername + " of the " + teamname);
+            System.Console.WriteLine(weekKD);
+            System.Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Weekley KD Calculator.cs
@@ -0,0 +1,65 @@
+﻿
+
+namespace Weekly_KD_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string teamname;
+            string playername;
+
+            int age;
+
+            float match1kills;
+            float match2kills;
+            float match1deaths;
+            float match2deaths;
+
+            
+            bool canplayasdps;
+
+            System.Console.Write("Input team name. ");
+            teamname = System.Console.ReadLine();
+
+            System.Console.Write("Input player name. ");
+            playername = System.Console.ReadLine();
+
+            System.Console.Write("Input player age. ");
+            age = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Are they a dps player? ");
+           bool dpsplayer = System.Console.ReadLine().ToLower().StartsWith("y");
+
+            System.Console.Write("Input match 1 kills. ");
+            match1kills = float.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Input match 1 deaths. ");
+            match1deaths = float.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Input match 2 kills. ");
+            match2kills = float.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Input match 2 deaths. ");
+            match2deaths = float.Parse(System.Console.ReadLine());
+
+            float weekKD = (match1kills + match2kills) / (match1deaths + match2deaths);
+
+            canplayasdps = dpsplayer && age >= 18;
+
+            System.Console.WriteLine("Weekly K/D results for " + playername + " of the " + teamname);
+            System.Console.WriteLine(canplayasdps);
+            System.Console.WriteLine(weekKD);
+            System.Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
