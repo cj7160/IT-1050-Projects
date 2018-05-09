2ab5fd51c1eb77c9b8a272dc1d3c683ec2b23ae7
cj7160 <35666779+cj7160@users.noreply.github.com>
2018-05-08 21:02:40 -0400

    Final
    The Final

---
+++ The Final/The Final/Elevator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Final
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int Index)
+        {
+            Occupants[Index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            return Passenger.TotalWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            return Passenger.TotalWeight > this.MaxWeight;
+
+        }
+
+    }
+}
---
+++ The Final/The Final/Passenger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Final
+{
+    class Passenger
+    {
+        private string Name;
+        private double Weight;
+        public static double TotalWeight;
+
+        
+        public Passenger(string name, double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+
+            Passenger.TotalWeight += this.Weight;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public double GetWeight()
+        {
+            return this.Weight;
+        }
+
+       
+    }
+}
